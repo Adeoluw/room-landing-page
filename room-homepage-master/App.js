99aac383a517671038d91334c6a8522407f6da2e
Adeolu <afolabiadeolu36@gmail.com>
2023-08-13 23:54:00 +0100

    update...

--- room-homepage-master/App.js
+++ room-homepage-master/App.js
@@ -1,95 +1,129 @@
 const leftArrow = document.getElementById("left-img");
 const rightArrow = document.getElementById("right-img");
-const textSection = document.getElementsByClassName("text-section")[0]
+const textSection = document.getElementsByClassName("text-section")[0];
 // const textHead = document.getElementById("text-head")
 // const textPara = document.getElementById("text-para")
-const imgSection = document.getElementsByClassName("img-section")[0]
-const secondTextHead = document.getElementsByClassName("two")[0]
-const secondTextPara = document.getElementsByClassName("two")[1]
-const thirdTextHead = document.getElementsByClassName("three")[0]
-const thirdTextPara = document.getElementsByClassName("three")[1]
-const firstTextHead = document.getElementsByClassName("one")[0]
-const firstTextPara = document.getElementsByClassName("one")[1]
+const imgSection = document.getElementsByClassName("img-section")[0];
+const secondTextHead = document.getElementsByClassName("two")[0];
+const secondTextPara = document.getElementsByClassName("two")[1];
+const thirdTextHead = document.getElementsByClassName("three")[0];
+const thirdTextPara = document.getElementsByClassName("three")[1];
+const firstTextHead = document.getElementsByClassName("one")[0];
+const firstTextPara = document.getElementsByClassName("one")[1];
+const firstImage = document.getElementsByClassName("img-one")[0];
+const secondImage = document.getElementsByClassName("img-two")[0];
+const thirdImage = document.getElementsByClassName("img-three")[0];
 
-
-
-
-let images = [
-  "./images/desktop-image-hero-1.jpg",
-  "./images/desktop-image-hero-2.jpg",
-  "./images/desktop-image-hero-3.jpg",
-];
 
 
 leftArrow.addEventListener("click", () => {
-    if (firstTextHead.style.display === "block" && secondTextHead.style.display === "none" && thirdTextHead.style.display === "none") {
-      secondTextHead.style.display = "none";
-      secondTextPara.style.display = "none";
-      thirdTextHead.style.display = "block";
-      thirdTextPara.style.display = "block";
-      firstTextHead.style.display = "none";
-        firstTextPara.style.display = "none";
-        imgSection.style.backgroundImage ="url(./images/desktop-image-hero-3.jpg)";
-    } else if (
-      firstTextHead.style.display === "none" &&
-      secondTextHead.style.display === "none" &&
-      thirdTextHead.style.display === "block"
-    ) {
-      secondTextHead.style.display = "block";
-      secondTextPara.style.display = "block";
-      thirdTextHead.style.display = "none";
-      thirdTextPara.style.display = "none";
-      firstTextHead.style.display = "none";
-        firstTextPara.style.display = "none";
-        imgSection.style.backgroundImage ="url(./images/desktop-image-hero-2.jpg)";
+  if (
+    firstTextHead.style.display === "block" &&
+    secondTextHead.style.display === "none" &&
+    thirdTextHead.style.display === "none"
+  ) {
+    secondTextHead.style.display = "none";
+    secondTextPara.style.display = "none";
+    thirdTextHead.style.display = "block";
+    thirdTextPara.style.display = "block";
+    firstTextHead.style.display = "none";
+    firstTextPara.style.display = "none";
+    thirdImage.style.opacity = "1";
+    // firstImage.style.opacity = "0";
+    secondImage.style.opacity = "0";
+      firstImage.style.animationName = "fadeout"
+      thirdImage.style.animationName = "fadein";
+    //   thirdImage.style.animationDelay = "1s"
+    // imgSection.style.backgroundImage ="url(./images/desktop-image-hero-3.jpg)";
+  } else if (
+    firstTextHead.style.display === "none" &&
+    secondTextHead.style.display === "none" &&
+    thirdTextHead.style.display === "block"
+  ) {
+    secondTextHead.style.display = "block";
+    secondTextPara.style.display = "block";
+    thirdTextHead.style.display = "none";
+    thirdTextPara.style.display = "none";
+    firstTextHead.style.display = "none";
+    firstTextPara.style.display = "none";
+    secondImage.style.opacity = "1";
+    firstImage.style.opacity = "0";
+    thirdImage.style.opacity = "0";
+    thirdImage.style.animationName = "fadeout";
+    secondImage.style.animationName = "fadein";
 
-    } else{
-      secondTextHead.style.display = "none";
-      secondTextPara.style.display = "none";
-      thirdTextHead.style.display = "none";
-      thirdTextPara.style.display = "none";
-      firstTextHead.style.display = "block";
-        firstTextPara.style.display = "block";
-        imgSection.style.backgroundImage ="url(./images/desktop-image-hero-1.jpg)";
-      console.log("bitch");
-    }
 
+    // imgSection.style.backgroundImage ="url(./images/desktop-image-hero-2.jpg)";
+  } else {
+    secondTextHead.style.display = "none";
+    secondTextPara.style.display = "none";
+    thirdTextHead.style.display = "none";
+    thirdTextPara.style.display = "none";
+    firstTextHead.style.display = "block";
+    firstTextPara.style.display = "block";
+    firstImage.style.opacity = "1";
+    secondImage.style.opacity = "0";
+      thirdImage.style.opacity = "0";
+      secondImage.style.animationName = "fadeout";
+      firstImage.style.animationName = "fadein";
 
-})
+    // imgSection.style.backgroundImage ="url(./images/desktop-image-hero-1.jpg)";
+    console.log("bitch");
+  }
+});
 
 rightArrow.addEventListener("click", () => {
-    if (firstTextHead.style.display === "block" && secondTextHead.style.display === "none" && thirdTextHead.style.display === "none") {
-      secondTextHead.style.display = "block";
-      secondTextPara.style.display = "block";
-      thirdTextHead.style.display = "none";
-      thirdTextPara.style.display = "none";
-      firstTextHead.style.display = "none";
-        firstTextPara.style.display = "none";
-        imgSection.style.backgroundImage ="url(./images/desktop-image-hero-2.jpg)";
+  if (
+    firstTextHead.style.display === "block" &&
+    secondTextHead.style.display === "none" &&
+    thirdTextHead.style.display === "none"
+  ) {
+    secondTextHead.style.display = "block";
+    secondTextPara.style.display = "block";
+    thirdTextHead.style.display = "none";
+    thirdTextPara.style.display = "none";
+    firstTextHead.style.display = "none";
+    firstTextPara.style.display = "none";
+    secondImage.style.opacity = "1";
+    thirdImage.style.opacity = "0";
+      firstImage.style.opacity = "0";
+      firstImage.style.animationName = "fadeout"
+      secondImage.style.animationName = "fadein"
+    // imgSection.style.backgroundImage ="url(./images/desktop-image-hero-2.jpg)";
+  } else if (
+    firstTextHead.style.display === "none" &&
+    secondTextHead.style.display === "block" &&
+    thirdTextHead.style.display === "none"
+  ) {
+    secondTextHead.style.display = "none";
+    secondTextPara.style.display = "none";
+    thirdTextHead.style.display = "block";
+    thirdTextPara.style.display = "block";
+    firstTextHead.style.display = "none";
+    firstTextPara.style.display = "none";
+    thirdImage.style.opacity = "1";
+    firstImage.style.opacity = "0";
+      secondImage.style.opacity = "0";
+      secondImage.style.animationName = "fadeout";
+      thirdImage.style.animationName = "fadein";
 
-    } else if (
-      firstTextHead.style.display === "none" &&
-      secondTextHead.style.display === "block" &&
-      thirdTextHead.style.display === "none"
-    ) {
-      secondTextHead.style.display = "none";
-      secondTextPara.style.display = "none";
-      thirdTextHead.style.display = "block";
-      thirdTextPara.style.display = "block";
-      firstTextHead.style.display = "none";
-        firstTextPara.style.display = "none";
-     imgSection.style.backgroundImage = "url(./images/desktop-image-hero-3.jpg)";
 
-    } else{
-      secondTextHead.style.display = "none";
-      secondTextPara.style.display = "none";
-      thirdTextHead.style.display = "none";
-      thirdTextPara.style.display = "none";
-      firstTextHead.style.display = "block";
-      firstTextPara.style.display = "block";
-    imgSection.style.backgroundImage ="url(./images/desktop-image-hero-1.jpg)";
-    }
+    //  imgSection.style.backgroundImage = "url(./images/desktop-image-hero-3.jpg)";
+  } else {
+    secondTextHead.style.display = "none";
+    secondTextPara.style.display = "none";
+    thirdTextHead.style.display = "none";
+    thirdTextPara.style.display = "none";
+    firstTextHead.style.display = "block";
+    firstTextPara.style.display = "block";
+    firstImage.style.opacity = "1";
+    thirdImage.style.opacity = "0";
+      secondImage.style.opacity = "0";
+      thirdImage.style.animationName = "fadeout";
+      firstImage.style.animationName = "fadein";
 
 
-})
 
+    // imgSection.style.backgroundImage ="url(./images/desktop-image-hero-1.jpg)";
+  }
+});
